faa00ea6d4e0c1e117f28b0645d8aab9141251ff
xelor <kingxelor@gmail.com>
2018-11-12 11:42:04 +0100

    added application-life-cycle

---
+++ WPF-Example/WPF-Example/ApplicationLifeCycle.cs
@@ -0,0 +1,25 @@
+﻿using bytePassion.Library.Essentials.WpfTools.Application;
+using System.Windows;
+
+namespace WPF_Example
+{
+    internal class ApplicationLifeCycle : IApplicationLifeCycle
+    {
+        public void Startup(StartupEventArgs startupEventArgs)
+        {
+            var mainWindow = new MainWindow();
+
+            mainWindow.Show();
+        }
+
+        public void SessionEnding(SessionEndingCancelEventArgs sessionEndingCancelEventArgs)
+        {
+            // Here I could prevent for the app to close
+        }
+
+        public void Exit(ExitEventArgs exitEventArgs)
+        {
+            // Here I could cleanup resources
+        }
+    }
+}
